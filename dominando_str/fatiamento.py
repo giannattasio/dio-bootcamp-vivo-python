f3ec1474afaff82ffe32201ab31936e0e76bccbc
Augusto Giannattasio <augustozika10@hotmail.com>
2024-06-24 20:46:16 -0300

    Adicionado Fatiamento de str

---
+++ dominando_str/fatiamento.py
@@ -0,0 +1,21 @@
+Nome = "Augusto Giannattasio Lopes"
+
+print(Nome[0])
+
+# conta do inicio até o 9 espaço
+print(Nome[:9])
+
+# Conta apartir do 8 espaço
+print(Nome[8:])
+
+# Conta do 8 espaço até o 21
+print(Nome[8:21])
+
+# o 2 nesse caso faz com que conte de duas em duas letras
+print(Nome[8:21:2])
+
+# Cria uma cópia da str
+print(Nome[:])
+
+# Copia a str de tras p frente
+print(Nome[:: -1])
