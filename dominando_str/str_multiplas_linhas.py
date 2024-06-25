bc266b109ea3251db66debe86aa3d0310bb58921
Augusto Giannattasio <augustozika10@hotmail.com>
2024-06-25 20:00:02 -0300

    Adicionado str Multiplas linhas

---
+++ dominando_str/str_multiplas_linhas.py
@@ -0,0 +1,27 @@
+Nome = "Augusto"
+Idade = 20
+Profissão = "Jovem aprendiz"
+Linguagem = "Python"
+Telefone = 11969423245
+
+mensagem = f"""
+Boa noite, meu nome é {Nome}.
+Eu tenho {Idade} anos.
+E atualmente estou estudando {Linguagem}
+Para contato meu telefone é {Telefone}
+"""
+print(mensagem)
+
+
+print(
+    """
+    ========== Giannattasio Burguer ==========
+
+    1 - Acessar nosso cardapio digital
+    2 - Reservar uma mesa para seu aniversário
+    3 - Falar com um de nossos atendentes
+    4 - Acesse nossas redes
+
+      Obrigado pela preferencia, volte sempre! :)
+
+      """)
