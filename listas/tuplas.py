ce8b53fa6e2d8a6ef7e209207dd5e2e3eac7a2f9
Augusto Giannattasio <augustozika10@hotmail.com>
2024-07-02 20:50:30 -0300

    Adicionado Tuplas

---
+++ listas/tuplas.py
@@ -0,0 +1,16 @@
+carros = (
+    "Ferrari",
+    "Mustang",
+    "Lancer",
+    "Uno"
+)
+print(carros)
+
+palavras = tuple("paralelepipedo" "carro")
+print(palavras)
+
+numero_de_faltas = tuple([10, 8, 4, 14,])
+print(numero_de_faltas)
+
+continente = ("Ásia",)
+print(continente)
